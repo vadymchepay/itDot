889ec540b234e5f62d1a08ff30db4a5c7b6a3cc3
vadymchepay <vadymbychkov@gmail.com>
2018-06-12 20:05:04 +0300

    ready

--- script.js
+++ script.js
@@ -3,8 +3,19 @@ $(document).ready(function () {
    var heightSub =  $('.main_logo').height()/2;  
     
    $('.main_logo').on('mousemove', function(event){
-       var xPos = event.pageX;
-       var yPos = event.pageY;
+       var xPos = event.pageX - widthSub;
+       var yPos = event.pageY - heightSub;
+       
+       $('.circle').css({
+           'left' : -xPos/90 + 'px',
+           'top' : -yPos/90 + 'px'
+           
+       });
+       $('.dots').css({
+           'left' : xPos/70 + 'px',
+           'top' : yPos/70 + 'px'
+           
+       });
        
        console.log(xPos);
    })
