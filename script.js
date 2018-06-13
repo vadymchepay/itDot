1141c5eb44812cd4566df61fcb067a76a1f5a5a7
vadymchepay <vadymbychkov@gmail.com>
2018-06-13 23:17:39 +0300

    new detales

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
 $(document).ready(function () {
-   var widthSub = $('.main_logo').width()/2;
-   var heightSub =  $('.main_logo').height()/2;  
+   var widthSub = $('.main_header').width()/2;
+   var heightSub =  $('.main_header').height()/2;  
     
-   $('.main_logo').on('mousemove', function(event){
+   $('.main_header').on('mousemove', function(event){
        var xPos = event.pageX - widthSub;
        var yPos = event.pageY - heightSub;
        
-       $('.circle').css({
+       $('.double_bubble').css({
            'left' : -xPos/90 + 'px',
            'top' : -yPos/90 + 'px'
            
        });
-       $('.dots').css({
+       $('.cosmonaut').css({
            'left' : xPos/70 + 'px',
            'top' : yPos/70 + 'px'
            
        });
        
        console.log(xPos);
-   })
+   });
 });
