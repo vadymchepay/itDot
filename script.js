de9c70891b2077f234e7019020a6b6feea0a214a
vadymchepay <vadymbychkov@gmail.com>
2018-06-14 15:58:10 +0300

    animarion

--- script.js
+++ script.js
@@ -1,22 +1,41 @@
 $(document).ready(function () {
-   var widthSub = $('.main_header').width()/2;
-   var heightSub =  $('.main_header').height()/2;  
-    
-   $('.main_header').on('mousemove', function(event){
-       var xPos = event.pageX - widthSub;
-       var yPos = event.pageY - heightSub;
-       
-       $('.double_bubble').css({
-           'left' : -xPos/90 + 'px',
-           'top' : -yPos/90 + 'px'
-           
-       });
-       $('.cosmonaut').css({
-           'left' : xPos/70 + 'px',
-           'top' : yPos/70 + 'px'
-           
-       });
-       
-       console.log(xPos);
-   });
+    var widthSub = $('.main_header').width() / 2;
+    var heightSub = $('.main_header').height() / 2;
+
+    $('.main_header').on('mousemove', function (event) {
+        var xPos = event.pageX - widthSub;
+        var yPos = event.pageY - heightSub;
+//        $('.double_bubble').css({
+//            'left': $('.double_bubble').offset().left - xPos / 1200 });
+//        $('.bubble2').css({
+//            'left': $('.bubble2').offset().left + xPos / 1600});
+//        $('.bubble3').css({
+//            'left': $('.bubble3').offset().left - xPos / 1100});
+//        $('.bubble4').css({
+//            'left': $('.bubble4').offset().left + xPos / 1500});
+//        $('.bubble5').css({
+//            'left': $('.bubble5').offset().left - xPos / 1700});     
+//        $('.bubble6').css({
+//            'left': $('.bubble6').offset().left + xPos / 1400});        
+//        $('.bubble7').css({
+//            'left': $('.bubble7').offset().left - xPos / 1800});        
+//        $('.bubble8').css({
+//            'left': $('.bubble8').offset().left + xPos / 1800});
+        $('.cosmonaut').css({
+            'left': $('.cosmonaut').offset().left - xPos / 800
+            , 'top': $('.cosmonaut').offset().top + yPos / 800
+        });       
+        $('.rocket').css({
+            'left': $('.rocket').offset().left + xPos / 1200
+            , 'top': $('.rocket').offset().top - yPos / 1200
+        });        
+        $('.ufo').css({
+            'left': $('.ufo').offset().left - xPos / 1200
+            , 'top': $('.ufo').offset().top + yPos / 1200
+        });       
+        $('.satelite').css({
+            'left': $('.satelite').offset().left + xPos / 1200
+            , 'top': $('.satelite').offset().top - yPos / 1200
+        });
+    });
 });
