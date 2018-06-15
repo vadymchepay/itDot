114fab4e073dc8ef52266663f4b013f7e87ab69a
vadymchepay <vadymbychkov@gmail.com>
2018-06-15 10:18:47 +0300

    better animation

--- script.js
+++ script.js
@@ -11,37 +11,22 @@ $(document).ready(function () {
     $('.main_header').on('mousemove', function (event) {
         var xPos = event.pageX - widthSub;
         var yPos = event.pageY - heightSub;
-        //        $('.double_bubble').css({
-        //            'left': $('.double_bubble').offset().left - xPos / 1200 });
-        //        $('.bubble2').css({
-        //            'left': $('.bubble2').offset().left + xPos / 1600});
-        //        $('.bubble3').css({
-        //            'left': $('.bubble3').offset().left - xPos / 1100});
-        //        $('.bubble4').css({
-        //            'left': $('.bubble4').offset().left + xPos / 1500});
-        //        $('.bubble5').css({
-        //            'left': $('.bubble5').offset().left - xPos / 1700});     
-        //        $('.bubble6').css({
-        //            'left': $('.bubble6').offset().left + xPos / 1400});        
-        //        $('.bubble7').css({
-        //            'left': $('.bubble7').offset().left - xPos / 1800});        
-        //        $('.bubble8').css({
-        //            'left': $('.bubble8').offset().left + xPos / 1800});
-        $('.cosmonaut').css({
-            'left': $('.cosmonaut').offset().left - xPos / 800
-            , 'top': $('.cosmonaut').offset().top + yPos / 800
+
+        $('.move_cosmonaut').css({
+            'left': - xPos / 25
+            , 'top':yPos / 25
         });
-        $('.rocket').css({
-            'left': $('.rocket').offset().left + xPos / 1200
-            , 'top': $('.rocket').offset().top - yPos / 1200
+        $('.move_rocket').css({
+            'left':  xPos / 20
+            , 'top': - yPos / 20
         });
-        $('.ufo').css({
-            'left': $('.ufo').offset().left - xPos / 1200
-            , 'top': $('.ufo').offset().top + yPos / 1200
+        $('.move_ufo').css({
+            'left': - xPos / 20
+            , 'top': yPos / 20
         });
-        $('.satelite').css({
-            'left': $('.satelite').offset().left + xPos / 1200
-            , 'top': $('.satelite').offset().top - yPos / 1200
+        $('.move_satelite').css({
+            'left': xPos / 15
+            , 'top':  - yPos / 15
         });
     });
 });
